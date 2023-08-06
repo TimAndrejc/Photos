e99f19d4771cc0d174e3de6968838b74fb068052
Tim <tim.andrejc02@gmail.com>
2023-08-06 18:49:31 +0200

    album

---
+++ album.php
@@ -0,0 +1,64 @@
+<?php
+include_once 'header.php';
+
+if(!isset($_SESSION['id'])){
+    header("Location: index.php");
+    exit();
+}
+if(!isset($_GET['album'])){
+    header("Location: index.php");
+    exit();
+}
+require_once 'connection.php';
+$query = "SELECT* FROM albums WHERE id = ?";
+$stmt = $pdo->prepare($query);
+$stmt->execute([$_GET['album']]);
+$album = $stmt->fetch();
+if(!$album){
+    header("Location: index.php");
+    exit();
+}
+
+$query = "SELECT u.username FROM users u JOIN album_user a ON u.id = a.user_id WHERE a.album_id = ?";
+$stmt = $pdo->prepare($query);
+$stmt->execute([$_GET['album']]);
+$users = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+$query = "SELECT username FROM users WHERE id = ?";
+$stmt = $pdo->prepare($query);
+$stmt->execute([$album['creator_id']]);
+$creator = $stmt->fetch(PDO::FETCH_COLUMN);
+
+?>
+<h1> <?php echo $album['name']?></h1>
+<p> <?php echo date("d.m.Y", strtotime($album['date_of_hangout']))?></p>
+<p> Created by <?php echo $creator ?></p>
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+ <div class="container" style ="margin-top:10px; text-align: -webkit-center;">
+    <div class="masonry-container">
+    <?php
+        $query ="SELECT * FROM pictures WHERE album_id = ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$_GET['album']]);
+        $pictures = $stmt->fetchAll();
+        foreach($pictures as $picture){
+            ?>
+            <div class="masonry-item">
+            <div class="row">
+                <div class="col-md-12">
+                     <img src="uploads/<?php echo $picture['located']?>" alt="error" class="image">
+                </div>
+            </div>
+        </div>
+        <?php
+        }
+    ?>  
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
--- index.php
+++ index.php
@@ -6,8 +6,37 @@ if(!isset($_SESSION['id'])){
     exit;
 }
 ?>
-Hello <?php echo $_SESSION['username'] ?>!
+<h2>Hello <?php echo $_SESSION['username'] ?>!</h2>
+<div class="row" style ="width:100%; text-align:center; text-align: -webkit-center;">
 
+<?php
+    require_once 'connection.php';
+    $query = "SELECT * FROM album_user WHERE user_id = ?";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$_SESSION['id']]);
+    $albums = $stmt->fetchAll();
+    foreach($albums as $album){
+        $query = "SELECT * FROM albums WHERE id = ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$album['album_id']]);
+        $album = $stmt->fetch();
+        ?>
+        <a href="album.php?album=<?php echo $album['id'] ?>" style ="text-decoration:none; color:black"><div class="col-sm-6 col-lg-4" style="padding: 15px;">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="uploads/<?php echo $album['main_pic']?>" alt="error">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $album['name']?></h5>
+                <p class="card-text"><?php echo date("d.m.Y", strtotime($album['date_of_hangout'])) ?></p>
+            </div>
+        </div>
+    </div>
+    </a>
+        <?php
+    }
+    
+    
+?>
+</div>
 <?php
 include_once("footer.php");
 ?>
